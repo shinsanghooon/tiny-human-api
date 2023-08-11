c189be5062ecbdb9813f4a9883803e4953eb9dc1
신상훈 <sanghoon@macbook-m1pro.local>
2023-08-11 14:07:37 +0900

    Initial commit

---
+++ src/main/java/com/tinyhuman/tinyhumanapi/TinyHumanApiApplication.java
@@ -0,0 +1,13 @@
+package com.tinyhuman.tinyhumanapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyHumanApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinyHumanApiApplication.class, args);
+    }
+
+}
