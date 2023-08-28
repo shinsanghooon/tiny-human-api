dc2b110b99a79856db7700bae4101ea365f0bbe6
sanghoon <shinsanghooon@gmail.com>
2023-08-29 00:58:41 +0900

    feature: [#14] 스프링 시큐리티 세션 관리 설정 변경
    - STATELESS: 스프링 시큐리티가 인증 관점에서 더 이상 세션을 사용하지 않는다. 보안과 관련된 처리 등에서는 세션을 사용할 수도 있다는 의미이다.

--- src/main/java/com/tinyhuman/tinyhumanapi/auth/config/AuthConfig.java
+++ src/main/java/com/tinyhuman/tinyhumanapi/auth/config/AuthConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -37,17 +38,19 @@ public class AuthConfig {
 
         http
                 .authorizeHttpRequests(authorize ->
-                    authorize
-                            .requestMatchers(HttpMethod.POST, "/api/v1/auth/login").permitAll()
-                            .requestMatchers(HttpMethod.POST, "/api/v1/users").permitAll()
-                            .requestMatchers(HttpMethod.GET, "/api/v1/users/email/duplicate-check").permitAll()
-                            .anyRequest().authenticated()
+                        authorize
+                                .requestMatchers(HttpMethod.POST, "/api/v1/auth/login").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/api/v1/users").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/v1/users/email/duplicate-check").permitAll()
+                                .anyRequest().authenticated()
                 )
                 .formLogin(AbstractHttpConfigurer::disable)
                 .logout(logout -> logout
                         .logoutUrl("/api/v1/auth/logout")
                         .deleteCookies("JSESSIONID")
                         .invalidateHttpSession(true))
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(Customizer.withDefaults())
                 .addFilterBefore(new TokenAuthenticationFilter(customTokenProvider), UsernamePasswordAuthenticationFilter.class)
