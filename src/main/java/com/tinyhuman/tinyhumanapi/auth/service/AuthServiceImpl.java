71589fad08d9e9336af9ddafd521fefa2c99a338
sanghoon <shinsanghooon@gmail.com>
2024-02-22 12:52:12 +0900

    feature: 구글 소셜 로그인 토큰 인증 기능 구현

--- src/main/java/com/tinyhuman/tinyhumanapi/auth/service/AuthServiceImpl.java
+++ src/main/java/com/tinyhuman/tinyhumanapi/auth/service/AuthServiceImpl.java
@@ -2,23 +2,31 @@ package com.tinyhuman.tinyhumanapi.auth.service;
 
 import com.tinyhuman.tinyhumanapi.auth.config.jwt.CustomTokenProvider;
 import com.tinyhuman.tinyhumanapi.auth.controller.port.AuthService;
+import com.tinyhuman.tinyhumanapi.auth.controller.port.dto.GoogleInfoResponse;
 import com.tinyhuman.tinyhumanapi.auth.domain.LoginRequest;
 import com.tinyhuman.tinyhumanapi.auth.domain.RefreshToken;
+import com.tinyhuman.tinyhumanapi.auth.domain.SocialLoginRequest;
 import com.tinyhuman.tinyhumanapi.auth.domain.TokenResponse;
 import com.tinyhuman.tinyhumanapi.auth.service.port.RefreshTokenRepository;
 import com.tinyhuman.tinyhumanapi.common.exception.ResourceNotFoundException;
+import com.tinyhuman.tinyhumanapi.common.exception.UnauthorizedAccessException;
 import com.tinyhuman.tinyhumanapi.user.domain.User;
+import com.tinyhuman.tinyhumanapi.user.enums.UserStatus;
 import com.tinyhuman.tinyhumanapi.user.service.port.UserRepository;
 import lombok.Builder;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
 
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
@@ -66,6 +74,7 @@ public class AuthServiceImpl implements AuthService {
         return tokenResponse;
     }
 
+
     private RefreshToken generateNewRefreshToken(User user, TokenResponse tokenResponse) {
         Optional<RefreshToken> findRefreshToken = refreshTokenRepository.findByUserId(user.id());
 
@@ -89,6 +98,62 @@ public class AuthServiceImpl implements AuthService {
                     log.error("ResourceNotFoundException(User) - userEmail:{}", email);
                     return new ResourceNotFoundException("User", " " + email);
                 });
+    }
+
+    @Override
+    public TokenResponse googleLogin(SocialLoginRequest socialLoginRequest) {
+        String email = socialLoginRequest.email();
+        String accessToken = socialLoginRequest.socialAccessToken();
+        String name = socialLoginRequest.name();
+        String photoUrl = socialLoginRequest.photoUrl();
+
+
+        RestTemplate restTemplate = new RestTemplate();
+        Map<String, String> map = new HashMap<>();
+        map.put("access_token", accessToken);
+        ResponseEntity<GoogleInfoResponse> infoResponse = restTemplate.postForEntity("https://www.googleapis.com/oauth2/v1/tokeninfo",
+                map, GoogleInfoResponse.class);
+
+        System.out.println("infoResponse.getBody() = " + infoResponse.getBody().toString());
+
+        String responseEmail = infoResponse.getBody().email();
+        boolean isVerifiedEmail = infoResponse.getBody().verified_email();
+
+        if (!isVerifiedEmail) {
+            log.error("Google Login Fail to Verify (User) - userEmail:{}", email);
+            throw new UnauthorizedAccessException("구글 로그인", email);
+        }
+
+        if (!email.equals(responseEmail)) {
+            log.error("Google Login Fail to Verify (User) - userEmail:{}", email);
+            throw new UnauthorizedAccessException("구글 로그인", email);
+        }
+
+        // TODO: user 생성 or 기존에 있는지 여부 확인
+        boolean isEmailExists = userRepository.existsByEmail(email);
+
+        User user;
+        if (isEmailExists) {
+            user = userRepository.findByEmail(email).get();
+        } else {
+            user = userRepository.save(User.builder()
+                    .name(name)
+                    .email(email)
+                    .password(accessToken)
+                    .status(UserStatus.ACTIVE)
+                    .build());
+        }
+
+        System.out.println("user.email() = " + user.email());
+        System.out.println("user.name() = " + user.name());
+
+        TokenResponse tokenResponse = customTokenProvider.generationToken(user, Duration.ofHours(2));
+
+        RefreshToken newRefreshToken = generateNewRefreshToken(user, tokenResponse);
+
+        refreshTokenRepository.save(newRefreshToken);
+
+        return tokenResponse;
     }
 }
 
