b6ec0d158396d0d60dbcd27ab0f8ac76ed476e2f
sanghoon <shinsanghooon@gmail.com>
2024-03-21 18:29:05 +0900

    feature: HealthCheck API 추가

--- src/main/java/com/tinyhuman/tinyhumanapi/common/controller/BasicController.java
+++ src/main/java/com/tinyhuman/tinyhumanapi/common/controller/BasicController.java
@@ -2,14 +2,17 @@ package com.tinyhuman.tinyhumanapi.common.controller;
 
 
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @Tag(name = "BasicController", description = "기본적인 처리하기 위한 컨트롤러입니다.")
 public class BasicController {
 
     @GetMapping("health")
+    @ResponseStatus(HttpStatus.OK)
     public String health() {
         return "I'm healthy!";
     }
